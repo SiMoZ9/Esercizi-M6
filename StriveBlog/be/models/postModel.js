bc723df9b667007175e55ba932a6e8785f5978d3
SiMoZ9 <simonejonas08@gmail.com>
2023-10-06 18:52:49 +0200

    posts section - be

--- StriveBlog/be/models/postModel.js
+++ StriveBlog/be/models/postModel.js
@@ -4,41 +4,32 @@ const PostSchema = new mongoose.Schema({
 
     category: {
       type: String,
-      required: true
     },
 
     title: {
         type: String,
-        required: true
     },
     cover: {
         type: String,
-        required: true
     },
     readTime: {
         value: {
             type: Number,
-            required: true
         },
         unit:{
             type: String,
-            required: true
         },
     },
     author: {
         name: {
             type: String,
-            required: true
         },
         avatar: {
             type: String,
-            required: true
         },
-        required: true
     },
     content: {
         type: String,
-        required: true
     }
 }, {timestamps: true, strict: true})
 
--- StriveBlog/be/routes/posts.js
+++ StriveBlog/be/routes/posts.js
@@ -3,8 +3,8 @@ const postsModel = require('../models/postModel')
 const post = express.Router()
 
 // all posts
-post.get('/blogPosts', (req, res) => {
-    const posts = new postsModel.find()
+post.get('/blogPosts', async (req, res) => {
+    const posts = await postsModel.find()
     try {
 
         if(!posts) {
@@ -30,6 +30,73 @@ post.get('/blogPosts', (req, res) => {
             }
         )
     }
+})
+
+post.get('/blogPosts/:id', async (req, res) => {
+  const {id} = req.params
+  const posts = await postsModel.findById(id)
+  try {
+    if (!posts) {
+       res.status(404).send(
+                {
+                    statusCode: 404,
+                    message: "Posts not found"
+                }
+            )
+
+    }
+
+    res.status(200).send(
+                {
+                    statusCode: 200,
+                    message: "Here's your post",
+        posts
+                }
+            )
+
+    } catch (e) {
+         res.status(500).send(
+                {
+                    statusCode: 500,
+                    message: "Posts not found"
+                }
+            )
+    }
+  
+})
+
+post.post('/blogPosts/create', async (req, res) => {
+    const newPost = new postsModel({
+        category: req.body.category,
+        title: req.body.title,
+        cover: req.body.cover,
+        readTime: {
+            value: req.body.readTime.value,
+            unit: req.body.readTime.unit
+        },
+        author: {
+            name: req.body.author.name,
+            avatar: req.body.author.avatar
+        },
+        content: req.body.content
+    })
+
+    try {
+
+        const postSave = await newPost.save()
+
+        res.status(201).send({
+            statusCode: 201,
+            message: 'Post published successfully',
+            postSave
+        })
+    } catch (err) {
+        res.status(500).send({
+            statusCode: 500,
+            message: "Internal server error"
+        })
+    }
+
 })
 
 module.exports = post
